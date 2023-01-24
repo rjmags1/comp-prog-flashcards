3b816a8311a5a529aedf41e105ae3d12398ad9d9
RJ Maganini <maganinirj@gmail.com>
2023-01-24 16:12:17 -0600

    init

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,11 @@
+// don't cause command prompt window popup on windows
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
+fn main() {
+  tauri::Builder::default()
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
