86d0d9010fcbfa731bea6c31bcb8ce3df413a5d8
RJ Maganini <maganinirj@gmail.com>
2023-02-02 12:10:39 -0600

    cards page filter controller 2

--- src/components/cards/TagFilter.tsx
+++ src/components/cards/TagFilter.tsx
@@ -1,38 +1,64 @@
-import { LegacyRef, useContext } from "react"
-import { AppLevelContext, TagFilterProps } from "../../types"
+import { LegacyRef, useContext, useState } from "react"
+import {
+    AppLevelContext,
+    FilterLevelContext,
+    TagFilterProps,
+} from "../../types"
 import { AppContext } from "../../app/App"
 import useOutsideClickHandler from "../../hooks"
 import TagFilterOption from "./TagFilterOption"
+import { FilterContext } from "./FilterController"
 
 function TagFilter({ tagType }: TagFilterProps) {
     const { tags } = useContext(AppContext) as AppLevelContext
+    const { filterTags } = useContext(FilterContext) as FilterLevelContext
     const { ref, render, setRender } = useOutsideClickHandler(false)
+    const [searchText, setSearchText] = useState("")
+
+    const options = Array.from(tags.entries())
+        .filter(
+            ([_, tag]) =>
+                tag.type === tagType && tag.name.indexOf(searchText) > -1
+        )
+        .map(([id, tag]) => <TagFilterOption key={id} id={id} tag={tag} />)
+    options.sort(
+        (tag1, tag2) =>
+            Number(filterTags.has(tag2.props.id)) -
+            Number(filterTags.has(tag1.props.id))
+    )
 
     return (
         <>
             <div
                 style={render ? { borderRadius: ".375rem .375rem 0 0" } : {}}
                 ref={ref as LegacyRef<HTMLDivElement>}
-                className="relative min-w-[18%] shrink rounded-md bg-white px-2"
+                className="relative min-w-[20%] shrink rounded-md bg-white px-2"
             >
-                <div
-                    onClick={() => setRender(true)}
-                    className="flex justify-between pt-1"
-                >
-                    <span>{tagType}s</span>
-                    <span className="mt-[1.5px] text-sm">▼</span>
-                </div>
+                {!render && (
+                    <div
+                        onClick={() => setRender(true)}
+                        className="flex justify-between pt-1"
+                    >
+                        <span>{tagType}s</span>
+                        <span className="mt-[1.5px] text-sm">▼</span>
+                    </div>
+                )}
+                {render && (
+                    <input
+                        placeholder="Search"
+                        value={searchText}
+                        autoFocus
+                        className="flex w-full justify-between rounded-md pt-1 text-black outline-none"
+                        onChange={(e) => setSearchText(e.target.value)}
+                    ></input>
+                )}
                 {render && (
                     <div
                         className="no-scrollbar absolute -ml-2 mt-[1px] 
                         max-h-[80vh] w-full overflow-y-scroll 
                         rounded-b-md bg-white"
                     >
-                        {Array.from(tags.entries())
-                            .filter(([_, tag]) => tag.type === tagType)
-                            .map(([id, tag]) => (
-                                <TagFilterOption key={id} id={id} tag={tag} />
-                            ))}
+                        {options}
                     </div>
                 )}
             </div>
