8133ab0b1e7326c242aea8ec7fa4e02e52b6ad17
RJ Maganini <maganinirj@gmail.com>
2023-02-03 14:56:27 -0600

    disable background buttons on modal and popup

--- src/components/general/Modal.tsx
+++ src/components/general/Modal.tsx
@@ -9,13 +9,15 @@ function Modal({ children, whiteText, widthVw, heightVh }: ModalProps) {
         marginTop: `${-(heightVh / 2)}vh`,
     }
     return (
-        <div
-            style={style}
-            className="no-scrollbar fixed top-[50%] left-[50%] z-20 flex 
-            flex-col items-center justify-center overflow-hidden overflow-y-scroll
-            rounded-md border border-gray-300 bg-stone-800 px-6 text-base drop-shadow-xl"
-        >
-            {children}
+        <div className="absolute top-0 bottom-0 left-0 right-0 z-10">
+            <div
+                style={style}
+                className="no-scrollbar fixed top-[50%] left-[50%] z-10 flex 
+                flex-col items-center justify-center overflow-hidden overflow-y-scroll
+                rounded-md border border-gray-300 bg-stone-800 px-6 text-base drop-shadow-xl"
+            >
+                {children}
+            </div>
         </div>
     )
 }
--- src/components/general/PopupMessage.tsx
+++ src/components/general/PopupMessage.tsx
@@ -10,49 +10,51 @@ function PopupMessage({
     whiteText,
 }: PopupMessageProps) {
     return (
-        <div
-            style={whiteText ? { color: "white" } : {}}
-            className="fixed top-[50%] left-[50%] z-20 -ml-[200px] 
+        <div className="absolute top-0 bottom-0 left-0 right-0 z-10">
+            <div
+                style={whiteText ? { color: "white" } : {}}
+                className="fixed top-[50%] left-[50%] z-20 -ml-[200px] 
             -mt-[100px] flex h-[200px] w-[400px] flex-col 
             items-center justify-center rounded-md border border-white
             bg-stone-800 px-6 text-base drop-shadow-xl"
-        >
-            {message}
-            {confirm ? (
-                <button
-                    className="mt-6 w-[100px] rounded-md border 
+            >
+                {message}
+                {confirm ? (
+                    <button
+                        className="mt-6 w-[100px] rounded-md border 
                   border-white bg-green-500 py-1 px-2 hover:bg-green-800"
-                    onClick={(e) => {
-                        e.stopPropagation()
-                        unrender()
-                    }}
-                >
-                    OK
-                </button>
-            ) : (
-                <div className="flex w-full items-center justify-center gap-x-5">
-                    <button
-                        className="mt-6 w-[100px] rounded-md border 
+                        onClick={(e) => {
+                            e.stopPropagation()
+                            unrender()
+                        }}
+                    >
+                        OK
+                    </button>
+                ) : (
+                    <div className="flex w-full items-center justify-center gap-x-5">
+                        <button
+                            className="mt-6 w-[100px] rounded-md border 
                     border-white bg-red-600 py-1 px-2 hover:bg-red-800"
-                        onClick={(e) => {
-                            e.stopPropagation()
-                            unrender(true)
-                        }}
-                    >
-                        Delete
-                    </button>
-                    <button
-                        className="mt-6 w-[100px] rounded-md border 
+                            onClick={(e) => {
+                                e.stopPropagation()
+                                unrender(true)
+                            }}
+                        >
+                            Delete
+                        </button>
+                        <button
+                            className="mt-6 w-[100px] rounded-md border 
                     border-white bg-gray-600 py-1 px-2 hover:bg-gray-800"
-                        onClick={(e) => {
-                            e.stopPropagation()
-                            unrender(false)
-                        }}
-                    >
-                        Cancel
-                    </button>
-                </div>
-            )}
+                            onClick={(e) => {
+                                e.stopPropagation()
+                                unrender(false)
+                            }}
+                        >
+                            Cancel
+                        </button>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
