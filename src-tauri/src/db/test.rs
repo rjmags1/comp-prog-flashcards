9b8e16785965856b91a4b22df394c55f938e0767
RJ Maganini <maganinirj@gmail.com>
2023-04-30 12:46:35 -0500

    start app context data loader unit test

--- src-tauri/src/db/test.rs
+++ src-tauri/src/db/test.rs
@@ -13,33 +13,111 @@ const TEST_MIGRATIONS: EmbeddedMigrations = embed_migrations!(
     "./src/db/test_migrations"
 );
 
+enum PreDefTagType {
+    Paradigm,
+    Concept,
+    Trick,
+}
+
+enum PreDefThemeType {
+    Normal,
+    Dark,
+}
+
 fn init_test_db() -> Result<
     SqliteConnection,
     Box<dyn Error + Send + Sync + 'static>
 > {
-    let mut conn = SqliteConnection::establish(TEST_DB_URL)?;
-    conn.run_pending_migrations(TEST_MIGRATIONS)?;
+    let mut conn = SqliteConnection::establish(TEST_DB_URL)?; // in memory sqlite db
+    conn.run_pending_migrations(TEST_MIGRATIONS)?; // just define empty entities
     let conn = conn;
     Ok(conn)
 }
 
-#[test]
-fn t() {
-    let conn = &mut init_test_db().unwrap();
-    use schema::{ User };
-    insert_into(User::table)
+fn insert_test_image(
+    conn: &mut SqliteConnection,
+    name: String,
+    path: String
+) -> Result<(), Box<dyn Error>> {
+    use schema::Image;
+    insert_into(Image::table)
+        .values((Image::name.eq(&name), Image::path.eq(&path)))
+        .execute(conn)?;
+    Ok(())
+}
+
+fn insert_test_tag(
+    conn: &mut SqliteConnection,
+    tag_type: PreDefTagType,
+    name: String,
+    content: Option<String>
+) -> Result<(), Box<dyn Error>> {
+    use schema::Tag;
+    let tag_type_id = match tag_type {
+        PreDefTagType::Paradigm => 1,
+        PreDefTagType::Concept => 2,
+        PreDefTagType::Trick => 3,
+    };
+    insert_into(Tag::table)
         .values((
-            User::username.eq("test"),
-            User::avatar.eq(1),
-            User::username.eq("test"),
-            User::theme.eq(1),
-            User::tagmask.eq(0),
-            User::hidediffs.eq(false),
+            Tag::type_.eq(tag_type_id),
+            Tag::name.eq(name),
+            Tag::content.eq(content),
         ))
-        .execute(conn)
-        .unwrap();
-    let should_eq_1 = User::table.select(User::id)
-        .load::<i32>(conn)
-        .unwrap()[0];
-    assert_eq!(should_eq_1, 1);
+        .execute(conn)?;
+    Ok(())
+}
+
+fn insert_test_user(
+    username: String,
+    avatar_id: i32,
+    theme_id: i32,
+    tagmask: i32,
+    hidediffs: bool
+) -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
+
+fn insert_test_source(name: String) -> Result<(), Box<dyn Error>> {
+    panic!();
+}
+
+#[test]
+fn test_load_app_context() {
+    // should load all user ids, names, tagmasks, hidediffs, avatar_path, their theme names
+    // should load all tag ids, type names, names, content
+    // should load all theme names
+    // should load all source id and names
+    let conn = &mut init_test_db().unwrap();
+    let test_image_path_1 = "test_path_1".to_string();
+    let test_image_path_2 = "test_path_2".to_string();
+    insert_test_image(
+        conn,
+        "test_name".to_string(),
+        test_image_path_1
+    ).unwrap();
+    insert_test_image(
+        conn,
+        "test_name".to_string(),
+        test_image_path_2
+    ).unwrap();
+
+    let test_tag_name_1 = "test_tag_1".to_string();
+    let test_tag_name_2 = "test_tag_2".to_string();
+    let test_tag_type_1 = PreDefTagType::Concept;
+    let test_tag_type_2 = PreDefTagType::Paradigm;
+    let test_tag_content_1 = Some("test content".to_string());
+    let test_tag_content_2: Option<String> = None;
+    insert_test_tag(
+        conn,
+        test_tag_type_1,
+        test_tag_name_1,
+        test_tag_content_1
+    ).unwrap();
+    insert_test_tag(
+        conn,
+        test_tag_type_2,
+        test_tag_name_2,
+        test_tag_content_2
+    ).unwrap();
 }
